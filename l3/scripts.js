e17317a54a3a7211a6d9f2911c0392c26df8e31b
dmitriy.belgiets <chegidim@gmail.com>
2019-10-25 14:03:10 +0300

    added some app skeleton and user actions

--- l3/scripts.js
+++ l3/scripts.js
@@ -1,10 +1,35 @@
-function TamagotchiCat(name) {
+function TamagotchiCat (name) {
   this.name = name
 
-  let happy = 0;
-  let health = 0;
-  let angry = 0;
-  let hungry = 0;
+  let health = 0
+  let mood = 0
+  let hungry = 0
+
+  this.init = function() {
+    let buttons = document.getElementsByClassName('action')
+    let cat = this
+
+    Array.from(buttons).forEach(function (button) {
+      button.addEventListener('click', function () {
+        let action = this.getAttribute("data-action")
+        let type = this.getAttribute("data-type")
+
+        cat.userAction(action, type)
+      })
+    })
+
+    setInterval(this.randomEvent, 5000)
+  }
+
+  this.userAction = function(action, type) {
+    console.log(action)
+    console.log(type)
+  }
+
+  this.randomEvent = function() {
+    console.log('random event')
+  }
 }
 
 let assTail = new TamagotchiCat('Mimi')
+assTail.init()
